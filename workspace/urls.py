c76db2f8d17d6ba9add8f81c6b4309f3cfe8be3b
ayyubyaqub <ayyubkhan4341@gmail.com>
2022-09-29 12:24:01 +0530

    project and task

--- workspace/urls.py
+++ workspace/urls.py
@@ -8,5 +8,9 @@ from rest_framework.routers import DefaultRouter
 urlpatterns=[
     path('workspace/',Work_space_view.as_view()),
     path('workspace/<int:pk>',Work_space_view.as_view()),
+    path('project/',project_view.as_view()),
+    path('project/<int:pk>',project_view.as_view()),
+    path('task/',task_view.as_view()),
+    path('task/<int:pk>',task_view.as_view()),
 
 ]
